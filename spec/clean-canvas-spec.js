15c1b4a945b975da8b8fef525bb1a326445d4c86
19khowes <72945859+19khowes@users.noreply.github.com>
2021-11-29 09:21:42 -0700

    name changing

--- spec/clean-canvas-spec.js
+++ spec/clean-canvas-spec.js
@@ -1,44 +1,44 @@
 'use babel';
 
-import CleanAlly from '../lib/clean-ally';
+import CleanCanvas from '../lib/clean-canvas';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('CleanAlly', () => {
+describe('CleanCanvas', () => {
   let workspaceElement, activationPromise;
 
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('clean-ally');
+    activationPromise = atom.packages.activatePackage('clean-canvas');
   });
 
-  describe('when the clean-ally:toggle event is triggered', () => {
+  describe('when the clean-canvas:toggle event is triggered', () => {
     it('hides and shows the modal panel', () => {
       // Before the activation event the view is not on the DOM, and no panel
       // has been created
-      expect(workspaceElement.querySelector('.clean-ally')).not.toExist();
+      expect(workspaceElement.querySelector('.clean-canvas')).not.toExist();
 
       // This is an activation event, triggering it will cause the package to be
       // activated.
-      atom.commands.dispatch(workspaceElement, 'clean-ally:toggle');
+      atom.commands.dispatch(workspaceElement, 'clean-canvas:toggle');
 
       waitsForPromise(() => {
         return activationPromise;
       });
 
       runs(() => {
-        expect(workspaceElement.querySelector('.clean-ally')).toExist();
+        expect(workspaceElement.querySelector('.clean-canvas')).toExist();
 
-        let cleanAllyElement = workspaceElement.querySelector('.clean-ally');
-        expect(cleanAllyElement).toExist();
+        let cleanCanvasElement = workspaceElement.querySelector('.clean-canvas');
+        expect(cleanCanvasElement).toExist();
 
-        let cleanAllyPanel = atom.workspace.panelForItem(cleanAllyElement);
-        expect(cleanAllyPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'clean-ally:toggle');
-        expect(cleanAllyPanel.isVisible()).toBe(false);
+        let cleanCanvasPanel = atom.workspace.panelForItem(cleanCanvasElement);
+        expect(cleanCanvasPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'clean-canvas:toggle');
+        expect(cleanCanvasPanel.isVisible()).toBe(false);
       });
     });
 
@@ -48,14 +48,14 @@ describe('CleanAlly', () => {
       // Attaching the workspaceElement to the DOM is required to allow the
       // `toBeVisible()` matchers to work. Anything testing visibility or focus
       // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
+      // workspaceElement to the DOM are genercanvas slower than those off DOM.
       jasmine.attachToDOM(workspaceElement);
 
-      expect(workspaceElement.querySelector('.clean-ally')).not.toExist();
+      expect(workspaceElement.querySelector('.clean-canvas')).not.toExist();
 
       // This is an activation event, triggering it causes the package to be
       // activated.
-      atom.commands.dispatch(workspaceElement, 'clean-ally:toggle');
+      atom.commands.dispatch(workspaceElement, 'clean-canvas:toggle');
 
       waitsForPromise(() => {
         return activationPromise;
@@ -63,10 +63,10 @@ describe('CleanAlly', () => {
 
       runs(() => {
         // Now we can test for view visibility
-        let cleanAllyElement = workspaceElement.querySelector('.clean-ally');
-        expect(cleanAllyElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'clean-ally:toggle');
-        expect(cleanAllyElement).not.toBeVisible();
+        let cleanCanvasElement = workspaceElement.querySelector('.clean-canvas');
+        expect(cleanCanvasElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'clean-canvas:toggle');
+        expect(cleanCanvasElement).not.toBeVisible();
       });
     });
   });
